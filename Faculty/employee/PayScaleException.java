6734900d0b14cbf855c06871c32a258aef7b4077
lloydhird <lloydhird@192-168-1-9.tpgi.com.au>
2017-05-23 01:06:31 +1000

    added PayScaleException class

---
+++ Faculty/employee/PayScaleException.java
@@ -0,0 +1,13 @@
+package employee;
+
+class PayScaleException extends Exception
+{
+
+   public PayScaleException() {}
+      
+   public PayScaleException(String message)
+   {
+      super(message);
+   }
+
+}
